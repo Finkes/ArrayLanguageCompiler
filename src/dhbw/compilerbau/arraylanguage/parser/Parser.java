5499c0de72338bf022d71010b8cde3b2c97823f9
Dominik Finkbeiner <finkes93@gmail.com>
2014-04-30 15:16:58 +0200

    parser working

--- src/dhbw/compilerbau/arraylanguage/parser/Parser.java
+++ src/dhbw/compilerbau/arraylanguage/parser/Parser.java
@@ -1,5 +1,220 @@
 package dhbw.compilerbau.arraylanguage.parser;
 
-public class Parser {
+import java.util.ArrayList;
+
+import dhbw.compilerbau.arraylanguage.scanner.Token;
+import dhbw.compilerbau.arraylanguage.scanner.Token.TokenType;
+
+public class Parser 
+{
+	protected ArrayList<Token> tokens;
+	
+	protected int tokenStreamPosition;
+	
+	
+	public Parser(ArrayList<Token> tokens)
+	{
+		this.tokens = tokens;
+	}
+	
+	protected Token nextToken()
+	{
+		int currentToken = tokenStreamPosition;
+		tokenStreamPosition++;
+		
+		return this.tokens.get(currentToken);
+	}
+	
+	protected boolean parseToken(TokenType tokenType)
+	{
+		int tokenPosition = getTokenStreamPosition();
+		
+		if(nextToken().getType() == tokenType)
+			return true;
+		
+		resetTokenStreamToPosition(tokenPosition);
+		return false;
+	}
+	
+	protected int getTokenStreamPosition()
+	{
+		return tokenStreamPosition;
+	}
+	
+	protected void resetTokenStreamToPosition(int position)
+	{
+		this.tokenStreamPosition = position;
+	}
+	
+	public boolean parse()
+	{
+		return parseAS();
+	}
+	
+	//template for a function
+	protected boolean parseTemplate()
+	{
+		int tokenPosition = getTokenStreamPosition();
+		
+		//TODO
+		
+		resetTokenStreamToPosition(tokenPosition);
+		return false;
+	}
+	
+	protected boolean parseAS()
+	{
+		int tokenPosition = getTokenStreamPosition();
+		
+		if(parseLBR() && parseElement() && parseElements() && parseRBR())
+			return true;
+		
+		resetTokenStreamToPosition(tokenPosition);
+		return false;
+	}
+	
+	protected boolean parseElements()
+	{	
+		while(parseElementAndComma())
+		{}
+		
+		return true;
+	}
+	
+	protected boolean parseElementAndComma()
+	{
+		int tokenPosition = getTokenStreamPosition();
+		
+		if(parseComma() && parseElement())
+			return true;
+		
+		resetTokenStreamToPosition(tokenPosition);
+		return false;
+	}
+	
+	protected boolean parseElement()
+	{
+		int tokenPosition = getTokenStreamPosition();
+		
+		//TODO
+		if(parseRange())
+			return true;
+		if(parseInteger())
+			return true;
+		if(parseFloat())
+			return true;
+		if(parseString())
+			return true;
+		if(parseNull())
+			return true;
+		
+		
+		resetTokenStreamToPosition(tokenPosition);
+		return false;
+	}
+	
+	protected boolean parseInteger()
+	{
+		int tokenPosition = getTokenStreamPosition();
+		
+		if(parseToken(TokenType.INTEGER))
+			return true;
+					
+		resetTokenStreamToPosition(tokenPosition);
+		return false;
+	}
+	
+	protected boolean parseString()
+	{
+		int tokenPosition = getTokenStreamPosition();
+		
+		if(parseToken(TokenType.STRING))
+			return true;
+		
+		resetTokenStreamToPosition(tokenPosition);
+		return false;
+	}
+	
+	protected boolean parseFloat()
+	{
+		int tokenPosition = getTokenStreamPosition();
+		
+		if(parseToken(TokenType.FLOAT))
+			return true;
+		
+		resetTokenStreamToPosition(tokenPosition);
+		return false;
+	}
+	
+	protected boolean parseRange()
+	{
+		int tokenPosition = getTokenStreamPosition();
+		
+		if(parseInteger() && parseRangeSymbol() && parseInteger())
+			return true;
+		
+		if(parseFloat() && parseRangeSymbol() && parseFloat())
+			return true;
+		
+		resetTokenStreamToPosition(tokenPosition);
+		return false;
+	}
+	
+	protected boolean parseRangeSymbol()
+	{
+		int tokenPosition = getTokenStreamPosition();
+		
+		if(parseToken(TokenType.RANGE))
+			return true;
+		
+		resetTokenStreamToPosition(tokenPosition);
+		return false;
+	}
+	
+	protected boolean parseLBR()
+	{
+		int tokenPosition = getTokenStreamPosition();
+		
+		if(parseToken(TokenType.LBR))
+			return true;
+		
+		resetTokenStreamToPosition(tokenPosition);
+		return false;
+	}
+	
+	protected boolean parseNull()
+	{
+		int tokenPosition = getTokenStreamPosition();
+		
+		if(parseToken(TokenType.NULL))
+			return true;
+		
+		resetTokenStreamToPosition(tokenPosition);
+		return false;
+	}
+	
+	protected boolean parseRBR()
+	{
+		int tokenPosition = getTokenStreamPosition();
+		
+		if(parseToken(TokenType.RBR))
+			return true;
+		
+		resetTokenStreamToPosition(tokenPosition);
+		return false;
+	}
+	
+	protected boolean parseComma()
+	{
+		int tokenPosition = getTokenStreamPosition();
+		
+		if(parseToken(TokenType.COMMA))
+			return true;
+		
+		resetTokenStreamToPosition(tokenPosition);
+		return false;
+	}
+	
+	
 
 }
--- src/dhbw/compilerbau/arraylanguage/scanner/Main.java
+++ src/dhbw/compilerbau/arraylanguage/scanner/Main.java
@@ -4,24 +4,41 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
 
+import dhbw.compilerbau.arraylanguage.parser.Parser;
+
 public class Main {
 
 	public static void main(String[] args)
 	{
-		 InputStream stream = new ByteArrayInputStream("[1...a,1.0^223,12,12,1as1,12.12..12.12,null,..,NULL,12..12.....]".getBytes());
+		    InputStream stream = new ByteArrayInputStream("[12,12,12.0,12.0,test,asjdhk,12..12,12.0..12.0]".getBytes());
 
+		    //InputStream stream = new ByteArrayInputStream("[,1.0^223,12,12,12.12..12..12,null,NULL]".getBytes());
+
+		    
 	        Scanner scanner = new Scanner(stream);
 
 	        ArrayList<Token> tokens = new ArrayList<Token>();
 
 	        Token token;
 
-	        try {
+	        try 
+	        {
 	            while ((token = scanner.getNextToken()) != null)
 	            {
 	                System.out.print(token.toString()+" ");
 	                tokens.add(token);
 	            }
+	            System.out.println("");
+	            System.out.println("starting parser...");
+	            
+	            //start parsing
+	            Parser parser = new Parser(tokens);
+	            
+	            if(parser.parse())
+	            	System.out.println("parsed successfull");
+	            else
+	            	System.out.println("parsed with failure");
+	            
 	        }
 	        catch (Exception e) {
 	            e.printStackTrace();
